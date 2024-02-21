70ff1fcfe083a2a2dc8e65a39d9cbdd31a03ca49
Maximilian Block <max@pool.math.tu-berlin.de>
2024-02-21 13:39:40 +0100

    upgrade to flake-utils

--- flake.nix
+++ flake.nix
@@ -7,32 +7,29 @@
       url = "github:neovim/neovim/stable?dir=contrib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ self, ... }:
-    let
-
-    # Override neovim with the one from the flake inputs
-    overlayFlakeInputs = prev: final: {
-      neovim = inputs.neovim.packages.x86_64-linux.neovim;
-    };
-
-    # Define a new package `mxNeovim`
-    overlayMxNeovim = prev: final: {
-      mxNeovim = import ./lib {
-        pkgs = final;
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      # Override neovim with the one from the flake inputs
+      overlayFlakeInputs = prev: final: {
+        neovim = inputs.neovim.packages.${system}.neovim;
       };
-    };
 
-    pkgs = import inputs.nixpkgs {
-      system = "x86_64-linux";
-      overlays = [ overlayFlakeInputs overlayMxNeovim ];
-    };
+      # Define a new package `mxNeovim`
+      overlayMxNeovim = prev: final: {
+        mxNeovim = import ./lib {
+          pkgs = final;
+        };
+      };
+
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [ overlayFlakeInputs overlayMxNeovim ];
+      };
     in {
-      packages.x86_64-linux.default = pkgs.mxNeovim;
-      apps.x86_64-linux.default = {
-        type = "app";
-        program = "${pkgs.mxNeovim}/bin/nvim";
-      };
-    };
+      packages.default = pkgs.mxNeovim;
+    }
+  );
 }
