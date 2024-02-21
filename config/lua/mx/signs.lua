140d7e7406fa53888661f5b9458542f13ee5edee
Maximilian Block <max@pool.math.tu-berlin.de>
2024-02-21 10:24:08 +0100

    update signs config

--- config/lua/mx/signs.lua
+++ config/lua/mx/signs.lua
@@ -14,15 +14,15 @@ Group.new("GitSignsDelete", c.red)
 
 signs.setup {
   signs = {
-    add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr" },
-    change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr" },
-    delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr" },
-    topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr" },
-    changedelete = { hl = "GitSignsDelete", text = "~", numhl = "GitSignsChangeNr" },
+    add = { hl = "GitSignsAdd", text = "│" },
+    change = { hl = "GitSignsChange", text = "│" },
+    delete = { hl = "GitSignsDelete", text = "_" },
+    topdelete = { hl = "GitSignsDelete", text = "‾" },
+    changedelete = { hl = "GitSignsDelete", text = "~" },
   },
 
   -- Highlights just the number part of the number column
-  numhl = true,
+  numhl = false,
 
   -- Highlights the _whole_ line.
   --    Instead, use gitsigns.toggle_linehl()
@@ -31,26 +31,6 @@ signs.setup {
   -- Highlights just the part of the line that has changed
   --    Instead, use gitsigns.toggle_word_diff()
   word_diff = false,
-
-  -- keymaps = {
-  --   -- Default keymap options
-  --   noremap = true,
-  --   buffer = true,
-  --
-  --   ["n <space>hd"] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'" },
-  --   ["n <space>hu"] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'" },
-  --
-  --   -- ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-  --   -- ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-  --   -- ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-  --   -- ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-  --   -- ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
-  -- },
-
-  current_line_blame_opts = {
-    delay = 2000,
-    virt_text_pos = "eol",
-  },
 }
 
 -- TODO: decide mappings for this
